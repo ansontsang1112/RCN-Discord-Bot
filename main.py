38e4dc0d2eb934d72542b3e92348b3b6806b3600
ansontsang1112 <ansont1112@gmail.com>
2021-10-08 19:00:58 +0800

    Fixed Status and added help sections.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ prefix = "$"
 token = "ODQxODc4NjE2Nzg2MjA2NzIw.YJtKjw._y09ei-sBAGZyG8DVzEyCR79ExA"
 
 bot = commands.Bot(command_prefix="$")
-
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -27,6 +27,7 @@ async def getMinecraftServerIP(ctx):
 async def getUserID(ctx):
     discordID = str(ctx.message.author)
     discordSystemID = str(ctx.message.author.id)
+    bot.remove_command('help')
     await ctx.channel.send("你的 DiscordID 是 " + discordID + "\n你的帳號 ID 是 " + discordSystemID)
 
 
@@ -47,23 +48,38 @@ async def apply(ctx, args="null"):
 @bot.command(name="status")
 async def serverStatus(ctx, args=""):
     stateCode, returnStatement = 0, ""
-    if not mcs.serverAvailable(args):
-        dataList = mcs.serverAvailable(args)
+    dataList = mcs.serverAvailable(args)
+
+    if args == "" and dataList["ip"] == "0.0.0.0":
+        returnStatement = "**請輸入伺服器名稱 :globe_with_meridians:**```- 新會市\n- HyperCity\n- MineLifeCity\n- 香港伊甸園\n- 伊織生存伺服器\n- 創造建築伺服器\n```" + "\n> 例如：" + prefix + "status HyperCity"
+    elif dataList["ip"] != "0.0.0.0" and dataList["ip"] != "127.0.0.1":
         stateCode = mcs.getServerStatus(dataList["ip"], int(dataList["port"]))
-
-    if args == "":
-        returnStatement = "請輸入伺服器名稱，新會市, HyperCity, MineLife City, 香港伊甸園, 伊織生存伺服器, 創造建築伺服器\n" + "例如：" + prefix + "status HyperCity"
+        if stateCode == 200:
+            returnStatement = "> :globe_with_meridians: " + args + "\n> 狀態：上線 :o:"
+        elif stateCode == 502:
+            returnStatement = "> :globe_with_meridians: " + args + "\n> 狀態：下線 :x:"
+        elif stateCode == 500:
+            returnStatement = "> :globe_with_meridians: 伺服器狀態檢測出現故障，請稍後再試。 "
     else:
-        args = "會員大堂"
-
-    if stateCode == 200:
-        returnStatement = args + " 正處於上線狀態。"
-    elif stateCode == 502:
-        returnStatement = args + " 正處於下線狀態。"
-    elif stateCode == 500:
-        returnStatement = "伺服器狀態檢測出現故障，請稍後再試。"
+        returnStatement = "> :globe_with_meridians: 你所輸入的伺服器名稱並不存在於 RCN 聯網當中。"
 
     await ctx.channel.send(returnStatement)
 
+
+@bot.command(name="help")
+async def embedHelp(ctx):
+    embed = discord.Embed(title="ResonanceCraft 機械人", url="https://www.resonancecraft.net",
+                          description="協助您解決 RCN 問題及了解更多關於 RCN 的資訊")
+    embed.set_author(name="[HN] 資訊科技部門")
+    embed.add_field(name=prefix+"help", value="查詢 RCN 機械人指令", inline=False)
+    embed.add_field(name=prefix+"ip", value="查詢 RCN Minecraft 聯網電腦版及手機版的登入 IP", inline=False)
+    embed.add_field(name=prefix+"myid", value="查詢自己的 Discord ID 及 帳號 ID", inline=False)
+    embed.add_field(name=prefix+"apply", value="用作申請職位或會員用途", inline=True)
+    embed.add_field(name="member", value="申請成為 RCN 會員", inline=True)
+    embed.add_field(name="staff", value="申請成為 RCN 職員", inline=True)
+    embed.add_field(name=prefix+"status", value="查詢聯網伺服器的狀況", inline=True)
+    embed.add_field(name="{servername}", value="輸入聯網伺服器名稱", inline=True)
+    embed.set_footer(text="ResonanceCraft Network 機器人使用指南 | Member of HyperGroup")
+    await ctx.send(embed=embed)
 
 bot.run(token)
--- mcs.py
+++ mcs.py
@@ -3,28 +3,24 @@ import json
 
 
 def serverAvailable(serverName=""):
-    serverList, count = ["新會市", "HyperCity", "MineLife City", "香港伊甸園", "伊織生存伺服器", "創造建築伺服器"], False
+    serverList, count, serverDict = ["新會市", "HyperCity", "MineLifeCity", "香港伊甸園", "伊織生存伺服器", "創造建築伺服器"], False, {}
     if serverName == "":
-        return {"ip": "10.68.55.27", "port": "58990"}
+        return {"ip": "0.0.0.0", "port": "00000"}
     else:
-        for serverName in serverList:
-            if serverName == serverList:
-                count = True
-                if serverName == serverList[0]:
-                    return {"ip": "10.68.55.25", "port": "64005"}
-                elif serverName == serverList[1]:
-                    return {"ip": "10.68.55.25", "port": "64006"}
-                elif serverName == serverList[2]:
-                    return {"ip": "10.68.55.25", "port": "64002"}
-                elif serverName == serverList[3]:
-                    return {"ip": "10.68.55.25", "port": "64003"}
-                elif serverName == serverList[4]:
-                    return {"ip": "10.68.55.27", "port": "56010"}
-                elif serverName == serverList[5]:
-                    return {"ip": "194.233.75.62", "port": "20001"}
-
-        if not count:
-            return False
+        if serverName == serverList[0]:
+            return {"ip": "10.68.55.25", "port": "64005"}
+        elif serverName == serverList[1]:
+            return {"ip": "10.68.55.25", "port": "64006"}
+        elif serverName == serverList[2]:
+            return {"ip": "10.68.55.25", "port": "64002"}
+        elif serverName == serverList[3]:
+            return {"ip": "10.68.55.25", "port": "64003"}
+        elif serverName == serverList[4]:
+            return {"ip": "10.68.55.27", "port": "56010"}
+        elif serverName == serverList[5]:
+            return {"ip": "194.233.75.62", "port": "20001"}
+        else:
+            return {"ip": "127.0.0.1", "port": "00000"}
 
 
 def getServerStatus(serverIP, port):
