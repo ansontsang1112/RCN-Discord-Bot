1f81e8ac08b4b05893fd7367c2e2419975ed0d46
ansontsang1112 <tch@s.hypernite.com>
2022-11-02 22:44:19 +0800

    Intent Update (20221102)

--- rcndiscordbot.py
+++ rcndiscordbot.py
@@ -6,8 +6,12 @@ from discord.ext import commands
 prefix = "$"
 token = "token"
 
-bot = commands.Bot(command_prefix="$")
+intents = discord.Intents.all()
+intents.members = True
+
+bot = commands.Bot(command_prefix="$", intents=intents)
 bot.remove_command('help')
+
 
 @bot.event
 async def on_ready():
@@ -51,7 +55,7 @@ async def serverStatus(ctx, args=""):
     dataList = mcs.serverAvailable(args)
 
     if args == "" and dataList["ip"] == "0.0.0.0":
-        returnStatement = "**請輸入伺服器名稱 :globe_with_meridians:**```- 新會市\n- HyperCity\n- MineLifeCity\n- 香港伊甸園\n- 伊織生存伺服器\n- 創造建築伺服器\n```" + "\n> 例如：" + prefix + "status HyperCity"
+        returnStatement = "**請輸入伺服器名稱 :globe_with_meridians:**```- HyperCity\n- 香港伊甸園\n- 伊織生存伺服器\n- 創造建築伺服器\n```" + "\n> 例如：" + prefix + "status HyperCity"
     elif dataList["ip"] != "0.0.0.0" and dataList["ip"] != "127.0.0.1":
         stateCode = mcs.getServerStatus(dataList["ip"], int(dataList["port"]))
         if stateCode == 200:
@@ -71,15 +75,16 @@ async def embedHelp(ctx):
     embed = discord.Embed(title="ResonanceCraft 機械人", url="https://www.resonancecraft.net",
                           description="協助您解決 RCN 問題及了解更多關於 RCN 的資訊")
     embed.set_author(name="[HN] 資訊科技部門")
-    embed.add_field(name=prefix+"help", value="查詢 RCN 機械人指令", inline=False)
-    embed.add_field(name=prefix+"ip", value="查詢 RCN Minecraft 聯網電腦版及手機版的登入 IP", inline=False)
-    embed.add_field(name=prefix+"myid", value="查詢自己的 Discord ID 及 帳號 ID", inline=False)
-    embed.add_field(name=prefix+"apply", value="用作申請職位或會員用途", inline=True)
+    embed.add_field(name=prefix + "help", value="查詢 RCN 機械人指令", inline=False)
+    embed.add_field(name=prefix + "ip", value="查詢 RCN Minecraft 聯網電腦版及手機版的登入 IP", inline=False)
+    embed.add_field(name=prefix + "myid", value="查詢自己的 Discord ID 及 帳號 ID", inline=False)
+    embed.add_field(name=prefix + "apply", value="用作申請職位或會員用途", inline=True)
     embed.add_field(name="member", value="申請成為 RCN 會員", inline=True)
     embed.add_field(name="staff", value="申請成為 RCN 職員", inline=True)
-    embed.add_field(name=prefix+"status", value="查詢聯網伺服器的狀況", inline=True)
+    embed.add_field(name=prefix + "status", value="查詢聯網伺服器的狀況", inline=True)
     embed.add_field(name="{servername}", value="輸入聯網伺服器名稱", inline=True)
     embed.set_footer(text="ResonanceCraft Network 機器人使用指南 | Member of HyperGroup")
     await ctx.send(embed=embed)
+
 
 bot.run(token)
